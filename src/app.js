aab9cca0adefe9740403080b1d6f2a5b79993f6a
yono-app <moreyonogames@gmail.com>
2026-06-30 02:04:57 +0530

    Fix

--- src/app.js
+++ src/app.js
@@ -19,7 +19,6 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://localhost:3000",
       "http://localhost:3001",
       "https://www.allyonoogames.com",
       "https://yonoworld.xyz",
