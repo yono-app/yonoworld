3747ebe2a8ae3bdb22e99d985d1a920a7dda07f5
yono-app <moreyonogames@gmail.com>
2026-07-02 04:44:05 +0530

    Update Robot.ts Issue

--- app/robots.ts
+++ app/robots.ts
@@ -9,7 +9,7 @@ export default function robots(): MetadataRoute.Robots {
         disallow: ["/api/", "/_next/", "/static/"],
       },
     ],
-    sitemap: "https://allyonoogames.com/sitemap.xml",
-    host: "https://allyonoogames.com",
+    sitemap: "https://www.yonoworld.xyz/sitemap_index.xml",
+    host: "https://www.yonoworld.xyz",
   };
 }
